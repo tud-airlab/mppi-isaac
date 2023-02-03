eaf802f76dea6cd6d13332b79f2ecad38f84a0bb
Max Spahn <m.spahn@tudelft.nl>
2023-02-03 11:29:40 +0100

    ft[init]: Adds exapmel skeleton for point_robot.
    Adds example for point robot with a blank MPPI planner.
    Adds urdfenvs as dependency.
    Imcreases python requirement to python3.8.

---
+++ examples/point_robot.py
@@ -0,0 +1,78 @@
+import gym
+import numpy as np
+from urdfenvs.robots.generic_urdf import GenericUrdfReacher
+from mppiisaac.planner.mppi import MPPIPlanner
+
+# MPPI to navigate a simple robot to a goal position
+
+def initalize_environment(render):
+    """
+    Initializes the simulation environment.
+
+    Adds an obstacle and goal visualizaion to the environment and
+    steps the simulation once.
+    
+    Params
+    ----------
+    render
+        Boolean toggle to set rendering on (True) or off (False).
+    """
+    robots = [
+        GenericUrdfReacher(urdf="../assets/point_robot.urdf", mode="vel"),
+    ]
+    env: UrdfEnv  = gym.make(
+        "urdf-env-v0",
+        dt=0.01, robots=robots, render=render
+    )
+    # Set the initial position and velocity of the point mass.
+    env.reset()
+    return env
+
+
+def set_planner(goal_position: np.ndarray):
+    """
+    Initializes the mppi planner for the point robot.
+
+    Params
+    ----------
+    goal_position: np.ndarray
+        The goal to the motion planning problem.
+    """
+    urdf = "../assets/point_robot.urdf"
+    planner = None
+    # planner = MPPIPlanner(config=config)
+    return planner
+
+
+def run_point_robot_urdf(n_steps=10000, render=True):
+    """
+    Set the gym environment, the planner and run point robot example.
+    The initial zero action step is needed to initialize the sensor in the
+    urdf environment.
+
+    Params
+    ----------
+    n_steps
+        Total number of simulation steps.
+    render
+        Boolean toggle to set rendering on (True) or off (False).
+    """
+    env = initalize_environment(render)
+    goal_position = np.array([2.0, 3.0])
+    planner = set_planner(goal_position)
+
+    action = np.array([0.0, 0.0, 0.0])
+    ob, *_ = env.step(action)
+
+    for _ in range(n_steps):
+        # Calculate action with the fabric planner, slice the states to drop Z-axis [3] information.
+        ob_robot = ob['robot_0']
+        #action[0:2] = planner.compute_action(
+        #    q=ob_robot["joint_state"]["position"][0:2],
+        #    qdot=ob_robot["joint_state"]["velocity"][0:2],
+        #)
+        ob, *_, = env.step(action)
+    return {}
+
+if __name__ == "__main__":
+    res = run_point_robot_urdf(n_steps=10000, render=True)
---
+++ mppiisaac/planner/mppi.py
@@ -0,0 +1,2 @@
+class MPPIPlanner(object):
+    pass
