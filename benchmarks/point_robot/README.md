f340f0a2cb7c1d50ed1c54dbe0e06140f9c8fcfe
Max Spahn <m.spahn@tudelft.nl>
2023-03-20 12:21:35 +0100

    ft[benchmark]: Adds first benchmark.

---
+++ benchmarks/point_robot/README.md
@@ -0,0 +1,9 @@
+# Benchmark for point robot
+
+If you want to compare mppi to other planners, you can use plannerbenchmark.
+As mppi is not part of the standard planners, you must export the following
+variable.
+
+```bash
+export LOCAL_PLANNER_BENCH_CUSTOM=/path/to/mppi_isaac/benchmarks/point_robot/mppi_planner 
+```
---
+++ benchmarks/point_robot/mppi_planner/mppi_planner_wrapper.py
@@ -0,0 +1,82 @@
+from omegaconf import OmegaConf
+from plannerbenchmark.generic.planner import Planner
+from mppiisaac.planner.isaacgym_wrapper import IsaacGymWrapper
+from mppiisaac.planner.mppi_isaac import MPPIisaacPlanner
+
+import torch
+
+class Objective(object):
+    def __init__(self, goal, device):
+        self.nav_goal = torch.tensor(goal, device=device)
+
+        self.w_nav = 1.0
+        self.w_obs = 0.5
+
+    def compute_cost(self, sim: IsaacGymWrapper):
+        dof_state = sim.dof_state
+        pos = torch.cat((dof_state[:, 0].unsqueeze(1), dof_state[:, 2].unsqueeze(1)), 1)
+        obs_positions = sim.obstacle_positions
+
+        nav_cost = torch.clamp(
+            torch.linalg.norm(pos - self.nav_goal, axis=1) - 0.05, min=0, max=1999
+        )
+
+        # sim.gym.refresh_net_contact_force_tensor(sim.sim)
+        # sim.net_cf
+
+        # This can cause steady state error if the goal is close to an obstacle, better use contact forces later on
+        obs_cost = torch.sum(
+            1 / torch.linalg.norm(obs_positions[:, :, :2] - pos.unsqueeze(1), axis=2),
+            axis=1,
+        )
+
+        return nav_cost * self.w_nav + obs_cost * self.w_obs
+
+
+
+class MPPIPlanner(Planner):
+
+    def __init__(self, exp, **kwargs):
+        super().__init__(exp, **kwargs)
+        self.cfg = kwargs['config']
+        self._config = OmegaConf.create(kwargs)
+
+        self.reset()
+
+    def setJointLimits(self, limits):
+        self._limits = limits
+
+    def setGoal(self, motionPlanningGoal):
+        cfg = OmegaConf.create(self.cfg)
+        goal_position = motionPlanningGoal.sub_goals()[0].position()
+        objective = Objective(goal_position, cfg.mppi.device)
+        if not hasattr(self, '_planner'):
+            self._planner = MPPIisaacPlanner(cfg, objective)
+
+
+    def setSelfCollisionAvoidance(self, r_body):
+        pass
+
+    def setObstacles(self, obstacles, r_body):
+        pass
+
+    def concretize(self):
+        pass
+
+    def save(self, folderPath):
+        file_name = folderPath + "/planner.yaml"
+        OmegaConf.save(config=self._config, f=file_name)
+
+    def computeAction(self, **kwargs):
+        ob = kwargs
+        obst = ob["FullSensor"]["obstacles"]
+        for o in obst.values():
+            o['type'] = 'sphere'
+
+        action = self._planner.compute_action(
+            q=ob["joint_state"]["position"],
+            qdot=ob["joint_state"]["velocity"],
+            obst=obst,
+        )
+        return action.numpy()
+
