edc8a6117278774466fbf6a6a9c0cc8b44af3306
cpezzato <cpezzato95@gmail.com>
2023-04-14 10:28:02 +0200

    Added heijn push files

---
+++ examples/heijn_push_client.py
@@ -0,0 +1,94 @@
+from mppiisaac.planner.mppi_isaac import MPPIisaacPlanner
+import hydra
+from omegaconf import OmegaConf
+import torch
+import zerorpc
+
+from mppiisaac.utils.config_store import ExampleConfig
+
+class Objective(object):
+    def __init__(self, cfg, device):
+        
+        # Tuning of the weights for baseline 2
+        self.w_robot_to_block_pos= 1#1
+        self.w_block_to_goal_pos=  20#5
+        self.w_block_to_goal_ort=  20#5
+        self.w_ee_hover=           55#15
+        self.w_ee_align=           .3#0.5
+        self.w_push_align=         45#0.5
+
+        # Task configration for comparison with baselines
+        self.ee_index = 6
+        self.block_index = 4
+        self.ort_goal_euler = torch.tensor([0, 0, 0], device=cfg.mppi.device)
+        self.ee_hover_height = 0.14
+
+        self.block_goal_pose_emdn_0 = torch.tensor([0.5, 0.3, 0.5, 0.0, 0.0, 0.0, 1.0], device=cfg.mppi.device)
+        self.block_goal_pose_emdn_1 = torch.tensor([0.4, 0.3, 0.5, 0, 0, -0.7071068, 0.7071068], device=cfg.mppi.device) # Rotation 90 deg
+
+        self.block_goal_pose_ur5_c = torch.tensor([0.65, 0, 0.5, 0, 0, 0, 1], device=cfg.mppi.device)
+        self.block_goal_pose_ur5_l= torch.tensor([0.7, 0.2, 0.5,  0, 0, 0.258819, 0.9659258 ], device=cfg.mppi.device) # Rotation 30 deg
+        self.block_goal_pose_ur5_r= torch.tensor([0.7, -0.2, 0.5,  0, 0, -0.258819, 0.9659258 ], device=cfg.mppi.device) # Rotation -30 deg
+
+        # Select goal according to test
+        self.block_goal_pose = torch.clone(self.block_goal_pose_ur5_l)
+        self.block_ort_goal = torch.clone(self.block_goal_pose[3:7])
+        self.goal_yaw = torch.atan2(2.0 * (self.block_ort_goal[-1] * self.block_ort_goal[2] + self.block_ort_goal[0] * self.block_ort_goal[1]), self.block_ort_goal[-1] * self.block_ort_goal[-1] + self.block_ort_goal[0] * self.block_ort_goal[0] - self.block_ort_goal[1] * self.block_ort_goal[1] - self.block_ort_goal[2] * self.block_ort_goal[2])
+
+        self.success = False
+        self.count = 0
+
+    def compute_metrics(self, block_pos, block_ort):
+
+        block_yaws = torch.atan2(2.0 * (block_ort[:,-1] * block_ort[:,2] + block_ort[:,0] * block_ort[:,1]), block_ort[:,-1] * block_ort[:,-1] + block_ort[:,0] * block_ort[:,0] - block_ort[:,1] * block_ort[:,1] - block_ort[:,2] * block_ort[:,2])
+        Ex = torch.abs(self.block_goal_pose[0]-block_pos[-1,0])
+        Ey = torch.abs(self.block_goal_pose[1]-block_pos[-1,1])
+        Etheta = torch.abs(block_yaws[-1] - self.goal_yaw)
+        return Ex, Ey, Etheta
+    
+    def compute_cost(self, sim):
+        r_pos = sim.rigid_body_state[:, self.ee_index, :3]
+        # r_ort = sim.rigid_body_state[:, self.ee_index, 3:7]
+       
+        block_pos = sim.root_state[:, self.block_index, :3]
+        # block_ort = sim.root_state[:, self.block_index, 3:7]
+
+        # Distances robot
+        robot_to_block = r_pos - block_pos
+        # robot_euler = pytorch3d.transforms.matrix_to_euler_angles(pytorch3d.transforms.quaternion_to_matrix(r_ort), "ZYX")   
+
+        # # Distances block
+        # block_to_goal =  self.block_goal_pose[0:2] - block_pos[:,0:2]
+        # # Compute yaw from quaternion with formula directly
+        # block_yaws = torch.atan2(2.0 * (block_ort[:,-1] * block_ort[:,2] + block_ort[:,0] * block_ort[:,1]), block_ort[:,-1] * block_ort[:,-1] + block_ort[:,0] * block_ort[:,0] - block_ort[:,1] * block_ort[:,1] - block_ort[:,2] * block_ort[:,2])
+        # #self.block_yaws = pytorch3d.transforms.matrix_to_euler_angles(pytorch3d.transforms.quaternion_to_matrix(block_ort), "ZYX")[:, -1]
+
+        # # Distance costs
+        robot_to_block_dist = torch.linalg.norm(robot_to_block[:, 0:2], axis = 1)
+        # block_to_goal_pos = torch.linalg.norm(block_to_goal, axis = 1)
+        # block_to_goal_ort = torch.abs(block_yaws - self.goal_yaw)
+
+        # push_align = torch.sum(robot_to_block[:,0:2]*block_to_goal, 1)/(robot_to_block_dist*block_to_goal_pos)+1
+        
+        total_cost = (
+            self.w_robot_to_block_pos * robot_to_block_dist
+            # + self.w_block_to_goal_pos * block_to_goal_pos
+            # + self.w_block_to_goal_ort * block_to_goal_ort
+            # + self.w_push_align * push_align
+        )
+
+        return total_cost
+
+@hydra.main(version_base=None, config_path="../conf", config_name="config_heijn_push")
+def run_heijn_robot(cfg: ExampleConfig):
+    # Note: Workaround to trigger the dataclasses __post_init__ method
+    cfg = OmegaConf.to_object(cfg)
+
+    objective = Objective(cfg, cfg.mppi.device)
+    prior = None
+    planner = zerorpc.Server(MPPIisaacPlanner(cfg, objective, prior))
+    planner.bind("tcp://0.0.0.0:4242")
+    planner.run()
+
+if __name__ == "__main__":
+    run_heijn_robot()
---
+++ examples/heijn_push_server.py
@@ -0,0 +1,196 @@
+from mppiisaac.planner.isaacgym_wrapper import IsaacGymWrapper
+import numpy as np
+import hydra
+from omegaconf import OmegaConf
+import torch
+import zerorpc
+from mppiisaac.utils.config_store import ExampleConfig
+from isaacgym import gymapi
+import time
+from examples.heijn_push_client import Objective
+import sys
+
+import io
+
+def torch_to_bytes(t: torch.Tensor) -> bytes:
+    buff = io.BytesIO()
+    torch.save(t, buff)
+    buff.seek(0)
+    return buff.read()
+
+
+def bytes_to_torch(b: bytes) -> torch.Tensor:
+    buff = io.BytesIO(b)
+    return torch.load(buff)
+
+def reset_trial(sim, init_pos, init_vel):
+    sim.stop_sim()
+    sim.start_sim()
+    sim.gym.viewer_camera_look_at(
+    sim.viewer, None, gymapi.Vec3(1.5, 2, 3), gymapi.Vec3(1.5, 0, 0)
+                )
+    sim.set_dof_state_tensor(torch.tensor([init_pos[0], init_vel[0], init_pos[1], init_vel[1], init_pos[2], init_vel[2]], device="cuda:0"))
+        
+@hydra.main(version_base=None, config_path="../conf", config_name="config_heijn_push")
+def run_heijn_robot(cfg: ExampleConfig):
+    # Note: Workaround to trigger the dataclasses __post_init__ method
+    cfg = OmegaConf.to_object(cfg)
+
+    sim = IsaacGymWrapper(
+        cfg.isaacgym,
+        cfg.urdf_file,
+        cfg.fix_base,
+        cfg.flip_visual,
+        num_envs=1,
+        ee_link=cfg.ee_link,
+        disable_gravity=cfg.disable_gravity,
+        viewer=True,
+    )
+
+    # Manually add table + block and restart isaacgym
+    obj_index = 0
+                #  l      w     h     mu      m     x    y
+    obj_set =  [[0.100, 0.100, 0.05, 0.300, 0.100, 2.00, 2.0],     # Baseline 1, pose 1
+                [0.100, 0.100, 0.05, 0.300, 0.100, 0.40, 0.],     # Baseline 1, pose 2
+                [0.116, 0.116, 0.06, 0.637, 0.016, 0.35, 0.],     # Baseline 2, A
+                [0.168, 0.237, 0.05, 0.232, 0.615, 0.38, 0.],     # Baseline 2, B
+                [0.198, 0.198, 0.06, 0.198, 0.565, 0.40, 0.],     # Baseline 2, C
+                [0.166, 0.228, 0.08, 0.312, 0.587, 0.39, 0.],     # Baseline 2, D
+                [0.153, 0.462, 0.05, 0.181, 0.506, 0.37, 0.],]    # Baseline 2, E
+    
+    obj_ = obj_set[obj_index][:]
+    table_dim = [0.8, 1.0, 0.108]
+    table_pos = [1, 1, table_dim[-1]/2]
+
+    additions = [
+        {
+            "type": "box",
+            "name": "table",
+            "size": table_dim,
+            "init_pos": table_pos,
+            "fixed": True,
+            "handle": None,
+        },
+        {
+            "type": "box",
+            "name": "block_to_push",
+            "size": [obj_[0], obj_[1], obj_[2]],
+            "init_pos": [obj_[5], obj_[6], table_dim[-1] + obj_[2] / 2],
+            "mass": obj_[4],
+            "fixed": False,
+            "handle": None,
+            "color": [4 / 255, 160 / 255, 218 / 255],
+            "friction": obj_[3]
+        }
+    ]
+
+    sim.add_to_envs(additions)
+
+    planner = zerorpc.Client()
+    planner.connect("tcp://127.0.0.1:4242")
+    print("Mppi server found!")
+
+    planner.add_to_env(additions)
+    
+    sim.gym.viewer_camera_look_at(
+        sim.viewer, None, gymapi.Vec3(1.5, 2, 3), gymapi.Vec3(1.5, 0, 0)
+    )
+    
+    init_pos = [0.0, 0., 1.]
+    init_vel = [0., 0., 0.]
+
+    sim.set_dof_state_tensor(torch.tensor([init_pos[0], init_vel[0], init_pos[1], init_vel[1], init_pos[2], init_vel[2]], device="cuda:0"))
+
+    # Helpers
+    count = 0
+    client_helper = Objective(cfg, cfg.mppi.device)
+    init_time = time.time()
+    block_index = 4
+    data_time = []
+    data_err = []
+    trial = 0 
+    timeout = 100
+    rt_factor_seq = []
+    data_rt = []
+
+    while trial < cfg.n_steps:
+        t = time.time()
+        # Reset state
+        planner.reset_rollout_sim(
+            torch_to_bytes(sim.dof_state[0]),
+            torch_to_bytes(sim.root_state[0]),
+            torch_to_bytes(sim.rigid_body_state[0]),
+        )
+        sim.gym.clear_lines(sim.viewer)
+        
+        # Compute action
+        action = bytes_to_torch(planner.command())
+        if torch.any(torch.isnan(action)):
+            print("nan action")
+            action = torch.zeros_like(action)
+
+        # Apply action
+        sim.set_dof_velocity_target_tensor(action)
+
+        # Step simulator
+        sim.step()
+
+        # Monitoring
+        # Evaluation metrics 
+        # ------------------------------------------------------------------------
+        if count > 10:
+            block_pos = sim.root_state[:, block_index, :3]
+            block_ort = sim.root_state[:, block_index, 3:7]
+
+            Ex, Ey, Etheta = client_helper.compute_metrics(block_pos, block_ort)
+            metric_1 = 1.5*(Ex+Ey)+0.01*Etheta
+            # print("Metric Baxter", metric_1)
+            # print("Ex", Ex)
+            # print("Ey", Ey)
+            # print("Angle", Etheta)
+            # Ex < 0.025 and Ey < 0.01 and Etheta < 0.05
+            # Ex < 0.05 and Ey < 0.025 and Etheta < 0.17
+            if Ex < 0.05 and Ey < 0.025 and Etheta < 0.17: 
+                print("Success")
+                final_time = time.time()
+                time_taken = final_time - init_time
+                print("Time to completion", time_taken)
+
+                reset_trial(sim, init_pos, init_vel)
+                
+                
+                init_time = time.time()
+                count = 0
+                data_rt.append(np.sum(rt_factor_seq) / len(rt_factor_seq))
+                data_time.append(time_taken)
+                data_err.append(np.float64(metric_1))
+                trial += 1
+
+            rt_factor_seq.append(cfg.isaacgym.dt/(time.time() - t))
+            # print(f"FPS: {1/(time.time() - t)} RT-factor: {cfg.isaacgym.dt/(time.time() - t)}")
+            
+            count = 0
+        else:
+            count +=1
+
+        if time.time() - init_time >= timeout:
+            reset_trial(sim, init_pos, init_vel)
+            init_time = time.time()
+            count = 0
+            data_time.append(-1)
+            data_err.append(-1)
+            data_rt.append(-1)
+            trial += 1
+
+        # Visualize samples
+        # rollouts = bytes_to_torch(planner.get_rollouts())
+        # sim.draw_lines(rollouts)
+        
+        # Print error of block
+        # pos = sim.root_state[0, -1][:2].cpu().numpy()
+        # goal = np.array([0.5, 0])
+        # print(f"L2: {np.linalg.norm(pos - goal)} FPS: {1/(time.time() - t)} RT-factor: {cfg.isaacgym.dt/(time.time() - t)}")
+    return {}
+
+if __name__ == "__main__":
+    res = run_heijn_robot()
