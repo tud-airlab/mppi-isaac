9a7f45017ec0ef921dbff597940e1f73046f3dcf
eliatrevisan <elia.trv@gmail.com>
2024-01-11 16:22:16 +0100

    adds jackal robot example with custom dynamics

---
+++ examples/jackal_robot_custom_dynamics.py
@@ -0,0 +1,146 @@
+import gym
+import numpy as np
+from urdfenvs.robots.generic_urdf import GenericDiffDriveRobot
+from urdfenvs.urdf_common.urdf_env import UrdfEnv
+from mppiisaac.planner.mppi_custom_dynamics import MPPICustomDynamicsPlanner
+import mppiisaac
+import hydra
+import yaml
+from yaml import SafeLoader
+from omegaconf import OmegaConf
+import os
+import torch
+from mpscenes.goals.static_sub_goal import StaticSubGoal
+from mppiisaac.utils.config_store import ExampleConfig
+import time
+from mppiisaac.dynamics.jackal_robot import jackal_robot_dynamics
+
+# MPPI to navigate a simple robot to a goal position
+
+
+class Objective(object):
+    def __init__(self, cfg, device):
+        self.nav_goal = torch.tensor(cfg.goal, device=cfg.mppi.device)
+
+    def compute_cost(self, state: torch.Tensor):
+        positions = state[:, 0:2]
+        rot_cost = torch.abs(state[:,5])
+        goal_dist = torch.linalg.norm(positions - self.nav_goal, axis=1)
+        return goal_dist * 1.0 + rot_cost * 0.05
+
+class Dynamics(object):
+    def __init__(self, cfg):
+        self.dt = cfg.dt
+
+    def simulate(self, states, control, t):
+        new_states, control = jackal_robot_dynamics(states, control, self.dt)
+        return (new_states, control)
+
+
+def initalize_environment(cfg) -> UrdfEnv:
+    """
+    Initializes the simulation environment.
+
+    Adds an obstacle and goal visualizaion to the environment and
+    steps the simulation once.
+
+    Params
+    ----------
+    render
+        Boolean toggle to set rendering on (True) or off (False).
+    """
+    with open(f'{os.path.dirname(mppiisaac.__file__)}/../conf/actors/jackal.yaml') as f:
+        heijn_cfg = yaml.load(f, Loader=SafeLoader)
+    urdf_file = f'{os.path.dirname(mppiisaac.__file__)}/../assets/urdf/' + heijn_cfg['urdf_file']
+    robots = [
+        GenericDiffDriveRobot(
+            urdf=urdf_file,
+            mode="vel",
+            actuated_wheels=[
+                "rear_right_wheel",
+                "rear_left_wheel",
+                "front_right_wheel",
+                "front_left_wheel",
+            ],
+            castor_wheels=[],
+            wheel_radius = 0.098,
+            wheel_distance = 2 * 0.187795 + 0.08,
+        ),
+    ]
+    env: UrdfEnv = gym.make("urdf-env-v0", dt=cfg.dt, robots=robots, render=cfg.render)
+    # Set the initial position and velocity of the point mass.
+    env.reset()
+    goal_dict = {
+        "weight": 1.0,
+        "is_primary_goal": True,
+        "indices": [0, 1],
+        "parent_link": 0,
+        "child_link": 1,
+        "desired_position": cfg.goal,
+        "epsilon": 0.05,
+        "type": "staticSubGoal",
+    }
+    goal = StaticSubGoal(name="simpleGoal", content_dict=goal_dict)
+    env.add_goal(goal)
+    return env
+
+
+def set_planner(cfg):
+    """
+    Initializes the mppi planner for the point robot.
+
+    Params
+    ----------
+    goal_position: np.ndarray
+        The goal to the motion planning problem.
+    """
+    # urdf = "../assets/point_robot.urdf"
+    objective = Objective(cfg, cfg.mppi.device)
+    dynamics = Dynamics(cfg)
+    planner = MPPICustomDynamicsPlanner(cfg, objective, dynamics.simulate)
+
+    return planner
+
+
+@hydra.main(version_base=None, config_path="../conf", config_name="config_jackal_robot_custom_dynamics.yaml")
+def run_point_robot(cfg: ExampleConfig):
+    """
+    Set the gym environment, the planner and run point robot example.
+    The initial zero action step is needed to initialize the sensor in the
+    urdf environment.
+
+    Params
+    ----------
+    n_steps
+        Total number of simulation steps.
+    render
+        Boolean toggle to set rendering on (True) or off (False).
+    """
+    env = initalize_environment(cfg)
+    planner = set_planner(cfg)
+
+    action = np.zeros(int(cfg.nx / 2))
+    ob, *_ = env.step(action)
+
+    for _ in range(cfg.n_steps):
+        # Calculate action with the fabric planner, slice the states to drop Z-axis [3] information.
+        ob_robot = ob["robot_0"]
+
+        t = time.time()
+
+        action = planner.compute_action(
+            q=ob_robot["joint_state"]["position"],
+            qdot=ob_robot["joint_state"]["velocity"],
+        )
+
+        print(f"Time: {(time.time() - t)} s")
+        
+        (
+            ob,
+            *_,
+        ) = env.step(action)
+    return {}
+
+
+if __name__ == "__main__":
+    res = run_point_robot()
